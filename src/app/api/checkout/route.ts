6ad9f870fd232e9418ef2c24488fdcff07ed29e6
Fernando Rojas <fer.rojas95@gmail.com>
2025-01-08 12:14:08 -0600

    📍 added zip codes for delivery

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -19,7 +19,7 @@ const predefinedHours = [
 ];
 
 export const POST = async (req: NextRequest) => {
-    const allowedZipCodes = ["12345"];
+    const allowedZipCodes = ["52930", "52934", "52936", "52937", "52938"];
     const today = new Date().toLocaleDateString("es-MX").split("T")[0];
 
     if (!process.env.STRIPE_SECRET_KEY) {
