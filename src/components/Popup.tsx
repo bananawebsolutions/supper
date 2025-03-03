683f2ebba8beb1246878519476ce286e06e49618
Fernando Rojas <96710775+Rojas9521@users.noreply.github.com>
2025-03-03 20:30:12 +0000

    ⚙️ fixed pop up condition to work on new browser

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -24,7 +24,7 @@ const Popup = () => {
         };
         fetchData();
 
-        if (hasRunPopup === "false") {
+        if (!hasRunPopup) {
             new Promise(() => {
                 setTimeout(() => {
                     setIsVisible(true);
